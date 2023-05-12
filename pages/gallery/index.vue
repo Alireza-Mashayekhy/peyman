90b7e14d9a01bf2d501623f275732d300b699f40
alireza <alirezamashayekhy2000@gmail.com>
2023-05-12 21:19:57 +0330

    add gallery page

---
+++ pages/gallery/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="gallery">
+    <div class="enGallery mont" v-if="language == 'en'">
+      <Header lang="eng" @change="change" />
+      <div class="topDiv">
+        <h3>Gallery</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas
+          purus viverra accumsan in nisl nisi Arcu cursus.
+        </p>
+      </div>
+      <div class="galleryList">
+        <div class="imageDiv" v-for="i in 9" :key="i">
+          <img src="/images/businessUnit.jpg" alt="gallery" />
+          <div class="text">لورم ایپسوم متن ساختگی با تولید</div>
+        </div>
+      </div>
+      <Footer lang="en" />
+    </div>
+    <div class="faGallery irancell" v-else>
+      <Header lang="fa" @change="change" />
+      <div class="topDiv">
+        <h3 class="mont">Gallery</h3>
+        <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+          از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
+          سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز.
+        </p>
+      </div>
+      <div class="galleryList">
+        <div class="imageDiv" v-for="i in 9" :key="i">
+          <img src="/images/businessUnit.jpg" alt="gallery" />
+          <div class="text">لورم ایپسوم متن ساختگی با تولید</div>
+        </div>
+      </div>
+      <Footer lang="fa" />
+    </div>
+    <MouseEffect />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      language: "",
+    };
+  },
+  mounted() {
+    this.language = this.$store.state.lang;
+  },
+  methods: {
+    change() {
+      this.$store.commit("change");
+    },
+  },
+  watch: {
+    "$store.state.lang"() {
+      this.language = this.$store.state.lang;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
